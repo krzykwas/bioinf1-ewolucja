79113a8ee4466085aa54b0b3b2bf2f22ca74aa72
Krzysztof Kwaśniewski <krzykwas@gmail.com>
2012-11-04 12:26:43 +0100

    e^A

--- BioinfEwolucja2012/src/main/java/pg/eti/kams/bioinfewolucja2012/Main.java
+++ BioinfEwolucja2012/src/main/java/pg/eti/kams/bioinfewolucja2012/Main.java
@@ -1,13 +1,24 @@
 package pg.eti.kams.bioinfewolucja2012;
 
+import org.jblas.DoubleMatrix;
+import org.jblas.MatrixFunctions;
+
 /**
  * Hello world!
  *
  */
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Main {
+
+    public static void main(String[] args) {
+        DoubleMatrix matrix = new DoubleMatrix(2, 2);
+        matrix.put(0, 1.3);
+        matrix.put(1, 2.4);
+        matrix.put(2, 0.7774);
+        matrix.put(3, 3.4);
+
+        System.out.println(matrix);
+
+        matrix = MatrixFunctions.expm(matrix);
+        System.out.println(matrix);
     }
 }
